6b282318b6d0115e39ade8ffb5c85bd9518d6513
Víctor Oliva <olivarra1@gmail.com>
2021-07-28 16:19:49 +0200

    can't connect to faunadb :(

--- netlify/functions/team.ts
+++ netlify/functions/team.ts
@@ -1,14 +1,27 @@
 import { Handler } from "@netlify/functions";
-import { Client } from "faunadb";
+import { Client, query } from "faunadb";
+
+type Response = Exclude<ReturnType<Handler>, void>;
 
 const handler: Handler = async (event, context) => {
-  const client = new Client({
-    secret: process.env.FAUNA_KEY,
-    domain: "db.fauna.com",
-    scheme: "https",
-  });
+  if (!process.env.FAUNA_KEY) {
+    throw new Error("Missing FaunaDB key");
+  }
+  const params = event.path.replace("/.netlify/functions/team", "").split("/");
 
-  console.log("event ->", process.env.ENV_TEST, event.httpMethod, event.path);
+  switch (event.httpMethod) {
+    case "GET":
+      if (params.length !== 2) return undefined;
+      return dbRun(async (client) => {
+        const team = await client.query(
+          query.Get(query.Ref(query.Collection("Teams")), Number(params[1]))
+        );
+        return {
+          statusCode: 200,
+          body: JSON.stringify(team),
+        };
+      });
+  }
 
   return {
     statusCode: 200,
@@ -16,4 +29,27 @@ const handler: Handler = async (event, context) => {
   };
 };
 
+async function dbRun(handler: (client: Client) => Promise<Response>) {
+  const client = new Client({
+    secret: process.env.FAUNA_KEY,
+  });
+
+  try {
+    return await handler(client);
+  } catch (ex) {
+    console.log(ex);
+    throw ex;
+  } finally {
+    client.close();
+  }
+}
+
 export { handler };
+
+setTimeout(() => {
+  dbRun(async (client) =>
+    client.query(
+      query.Get(query.Ref(query.Collection("Teams")), "305366791356416192")
+    )
+  ).catch(() => {});
+});
---
+++ serverless.ts
@@ -0,0 +1,68 @@
+import { Handler } from "@netlify/functions";
+import Koa from "koa";
+import dotenv from "dotenv";
+import "./netlify/functions/team";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  if (ctx.path.startsWith("/functions/")) {
+    const [, , functionName] = ctx.path.split("/");
+    const handler: Handler = require("./netlify/functions/" +
+      functionName).handler;
+
+    const headers = Object.fromEntries(
+      Object.entries(ctx.headers).filter(([, v]) => !Array.isArray(v))
+    ) as Record<string, string>;
+    const multiValueHeaders = Object.fromEntries(
+      Object.entries(ctx.headers).filter(([, v]) => Array.isArray(v))
+    ) as Record<string, string[]>;
+
+    const queryStringParameters = Object.fromEntries(
+      Object.entries(ctx.query).filter(([, v]) => !Array.isArray(v))
+    ) as Record<string, string>;
+    const multiValueQueryStringParameters = Object.fromEntries(
+      Object.entries(ctx.query).filter(([, v]) => Array.isArray(v))
+    ) as Record<string, string[]>;
+
+    const result = await handler(
+      {
+        body: ctx.body,
+        headers,
+        httpMethod: ctx.method,
+        isBase64Encoded: false,
+        path: ctx.path.replace("/functions/", "/.netlify/functions/"),
+        multiValueHeaders,
+        multiValueQueryStringParameters,
+        queryStringParameters,
+        rawQuery: "",
+        rawUrl: "",
+      },
+      null as any,
+      () => {}
+    );
+
+    if (!result) {
+      ctx.status = 500;
+    } else {
+      ctx.status = result.statusCode;
+      Object.entries(result.headers || {}).forEach(([name, value]) =>
+        ctx.set(name, String(value))
+      );
+      Object.entries(result.multiValueHeaders || {}).forEach(([name, value]) =>
+        ctx.set(
+          name,
+          value.map((v) => String(v))
+        )
+      );
+      ctx.response.body = result.body;
+    }
+  }
+
+  ctx.status = 404;
+});
+app.listen(8080);
